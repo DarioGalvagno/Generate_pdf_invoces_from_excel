e59370aca450b5a64afc3e98983e60677f673c7c
dario <darioggalvagno@outlook.com>
2024-09-06 08:13:56 +0200

    final committ

--- main.py
+++ main.py
@@ -15,33 +15,56 @@ for filepath in filepaths:
 
     # Extract filename and invoice number
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
 
     # Set title
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=10, txt=f"Invoice nr. {invoice_nr}", ln=True)
+    pdf.cell(w=50, h=10, txt=f"Invoice nr. {invoice_nr}", ln=1)
 
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=10, txt=f"Date: {date}", ln=1)
     # Add some space
     pdf.ln(10)
 
-    # Set table header
-    pdf.set_font(family="Times", size=12, style="B")
-    col_widths = [30, 60, 40, 30, 25]  # Adjust column widths based on your data
+    # Add a header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
-    # Assuming df.columns are the table headers
-    for header in df.columns:
-        pdf.cell(w=col_widths[df.columns.get_loc(header)], h=10, txt=header, border=1)
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
-    pdf.ln()
-
-    # Set table body
+    # Add sum of price
+    total_sum =df["total_price"].sum()
     pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
-    # Iterate through each row and add data to the PDF
-    for row in df.itertuples(index=False):
-        for idx, cell in enumerate(row):
-            pdf.cell(w=col_widths[idx], h=8, txt=str(cell), border=1)
-        pdf.ln()
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price for your purchased is {total_sum} Euros", ln=1)
+
+    # Add name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=30, h=8, txt="Bass player.com")  # Text
+    pdf.cell(w=10, h=8)  # Empty cell for space (adjust w for more/less space)
+    pdf.image("bassplayer.png", w=14)
 
     # Save PDF
     pdf.output(f"PDFs/{filename}.pdf")
+
+
